07af671aab971579217072b1ab9f873c4cddff21
Cinelli0730 <cyclesymphony70@gmail.com>
2024-03-04 20:17:53 +0900

    コミット漏れ

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,5 @@ import 'package:webapp_dev_mark01_flutter/page/at111/at111.dart';
 ///アプリケーションのmain関数
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SignInPage(),
-    );
-  }
+  runApp(const At111());
 }
